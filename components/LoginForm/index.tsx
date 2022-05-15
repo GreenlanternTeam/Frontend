da3f54a8ca56c896b703f08cfa8e528319b6f346
raymondanythings <akdfhr2@gmail.com>
2022-05-15 17:06:48 +0900

    브런치 초기화

--- components/LoginForm/index.tsx
+++ components/LoginForm/index.tsx
@@ -15,7 +15,7 @@ interface Props {
 const LoginForm = ({ register, handleSubmit }: Props) => {
 	const onValid = async (data: LoginType) => {
 		const res = await customAxios.post('/login', data)
-		console.log(res)
+		console.log(res``)
 	}
 	return (
 		<Wrrapper>
